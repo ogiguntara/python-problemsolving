8d26765f4cc04bb90dcaeec7e975fcc339681068
ogiguntara <ogiguntara@gmail.com>
2022-06-09 12:10:35 +0700

    diagonalDifference solved

--- warmUp.py
+++ warmUp.py
@@ -20,3 +20,14 @@ def compareTriplets(a,b):
 
 def aVeryBigSum(ar):
     return sum(ar)
+
+def diagonalDifference(arr):
+    primary = 0
+    secondary = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if i == j :
+                primary += arr[i][j]
+            if i+j == len(arr[i])-1:
+                secondary += arr[i][j]
+    return abs(primary-secondary)
