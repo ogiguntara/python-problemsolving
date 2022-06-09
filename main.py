2d4f9f5fe49dca281224610965871baa06450764
ogiguntara <ogiguntara@gmail.com>
2022-06-09 10:53:59 +0700

    comparetriplest solved

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from warmUp import *
 
 if __name__=="__main__":
     #a=solveMeFirst(2,3)
-    a1 = [1,2,3,4,10,11]
-    a = simpleArraySum(a1)
+    # a1 = [1,2,3,4,10,11]
+    # a = simpleArraySum(a1)
+    a1 = [5,6,7]
+    a2 = [3,6,10]
+    a=compareTriplets(a1,a2)
     print(a)
--- warmUp.py
+++ warmUp.py
@@ -5,3 +5,15 @@ def solveMeFirst(a,b):
 
 def simpleArraySum(ar):
     return sum(ar)
+
+def compareTriplets(a,b):
+    alicepoint = 0
+    bobpoint = 0
+    for pointa,pointb in zip(a,b):
+        if pointa > pointb:
+            alicepoint += 1
+        elif pointa < pointb :
+            bobpoint += 1
+        else :
+            pass
+    return alicepoint, bobpoint
