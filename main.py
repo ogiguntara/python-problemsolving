a2390d72b1e78ffae26e4536dacfac8f32409dd3
ogiguntara <ogiguntara@gmail.com>
2022-06-10 00:29:16 +0700

    adding sortstring

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!python3
 from warmUp import *
 from implementations import *
+from session6 import *
 
 if __name__=="__main__":
-    a1 = [73,67,40,33]
-    a=gradingStudents(a1)
+    # a1 = [73,67,40,33]
+    # a=gradingStudents(a1)
+    a1='Harta, reputasi, kekuatan. Orang yang memperoleh semuanya di dunia ini, Raja Bajak Laut, Gold Roger!'
+    a=sortString(a1)
     print(a)
     
