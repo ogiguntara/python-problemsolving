40005b18deb3ba7e0ed626af194fb0f406cf5aac
ogiguntara <ogiguntara@gmail.com>
2022-06-10 00:29:01 +0700

    add sortstring

---
+++ session6.py
@@ -0,0 +1,19 @@
+#!python3
+import string
+
+def sortString(a):
+    print(a)
+    #normalize string
+    new_string=a.translate(str.maketrans('','',string.punctuation))#remove punctuation
+    print(new_string)
+    #pecahkan kata dari kalimat
+    words = new_string.split(' ')#split by space
+    print(words)
+    print('total words : ',len(words))
+
+    #mengurutkan kata-kata
+    words.sort()
+    print(words)
+    print('\nHasil urutan sesuai abjad: ')
+    for word in words:
+        print(word)
